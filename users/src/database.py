9bc8b3dee79d28f1db77abe43b68d5521db76489
coldblade2000 <diegogranada7@gmail.com>
2023-08-16 01:39:19 -0500

    [fix] fixed database connection and session handling
    + creating the users table automatically

--- users/src/database.py
+++ users/src/database.py
@@ -3,11 +3,21 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@localhost:5432/db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@0.0.0.0:5432/db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, expire_on_commit=False)
 
 Base = declarative_base()
+
+
+# Dependency
+def get_session():
+    """Shares a single db session to be used throughout the service"""
+    session = SessionLocal()
+    try:
+        yield session
+    finally:
+        session.close()
--- users/src/main.py
+++ users/src/main.py
@@ -2,15 +2,17 @@
 
 from fastapi import FastAPI
 
+from src import models
+from src.database import engine, SessionLocal
 from src.users.router import router as users_router
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-
 app.include_router(users_router, prefix="/users", tags=["Users"])
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-
