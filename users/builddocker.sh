9867ad95085b7fcc9a50b50853eb08e9404a2f0a
coldblade2000 <diegogranada7@gmail.com>
2023-08-16 01:54:07 -0500

    [draft] began integrating environment variables

---
+++ users/builddocker.sh
@@ -0,0 +1,2 @@
+docker build
+docker run --env-file ./.env -p 127.0.0.1:8000:8000 users-micro:latest
--- users/src/constants.py
+++ users/src/constants.py
@@ -1,9 +1,15 @@
 """ File to store global constants and simple utils """
+import os
 from datetime import datetime
 
 TOKEN_LENGTH_BYTES = 128
 DEFAULT_SALT_LENGTH_BYTES = 32
 
+DB_USER = os.environ.get("DB_USER")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = os.environ.get("DB_PORT")
+DB_NAME = os.environ.get("DB_NAME")
 
 def datetime_to_str(date: datetime) -> str:
     """Returns a datetime as string in the correct ISO format"""
