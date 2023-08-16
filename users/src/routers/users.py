a2575ab3f73929ab498a8f13585d3f9ec83efe4a
coldblade2000 <diegogranada7@gmail.com>
2023-08-15 20:38:34 -0500

    [feat] implemented /users/ping route. Application works

--- users/src/routers/users.py
+++ users/src/routers/users.py
@@ -1,9 +1,9 @@
 """ /users router """
-from fastapi import APIRouter
+from fastapi import APIRouter, Response
 
 router = APIRouter()
 
 
-@router.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@router.get("/ping")
+async def ping():
+    return Response(content="pong", media_type="application/text")
