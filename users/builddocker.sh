e48716a3f0ae4d8947757b09f9dd24c1a1190bad
coldblade2000 <diegogranada7@gmail.com>
2023-08-20 17:17:58 -0500

    [feat] first instance of dockerized microservice successfully connecting to postgres container

--- users/builddocker.sh
+++ users/builddocker.sh
@@ -1,2 +1,2 @@
-docker build
-docker run --env-file ./.env -p 127.0.0.1:8000:8000 users-micro:latest
+docker build -t users-backend .
+docker run --env-file ./.env -p 127.0.0.1:8000:8000 --name users_backend --net packages-network users-backend
