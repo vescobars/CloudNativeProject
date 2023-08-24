4c9e142f0ae6fa94832a508d3e77896d64f6b0cd
veronica <v.escobar1@uniandes.edu.co>
2023-08-24 10:00:47 -0500

    [feat] added ping route functionality to check if the server is up.

--- routes/src/routes/router.py
+++ routes/src/routes/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response
 
 router = APIRouter()
 
@@ -6,3 +6,7 @@ router = APIRouter()
 @router.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+@router.get("/ping")
+async def ping():
+    return Response(content="pong", media_type="application/text")
