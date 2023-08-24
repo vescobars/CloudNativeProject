053d63d3ba40475b544eb69b4aae602aa616a386
veronica <v.escobar1@uniandes.edu.co>
2023-08-24 17:41:35 -0500

    [feat] Added date and uuid validation to schemas. Included missing type declaration in CreateRouteRequestSchema

--- routes/src/routes/schemas.py
+++ routes/src/routes/schemas.py
@@ -1,7 +1,16 @@
 """ Pydantic schemas for req and response bodies"""
+import uuid
+from typing import Annotated
+from datetime import datetime, timezone
+from pydantic import BaseModel, Field, AfterValidator
 
-from pydantic import BaseModel, Field, ValidationError
-from datetime import datetime
+
+def date_validation(d: datetime):
+    """Returns value error if date is before the current date"""
+    current_date = datetime.now()
+    if d < current_date:
+        raise ValueError("Date cannot be before the current date")
+    return d
 
 
 class CreateRouteRequestSchema(BaseModel):
@@ -11,13 +20,22 @@ class CreateRouteRequestSchema(BaseModel):
     flightId: str = Field(min_length=1)
     sourceAirportCode: str = Field(min_length=1)
     sourceCountry: str = Field(min_length=1)
-    destinyAirportCode: Field(min_length=1)
+    destinyAirportCode: str = Field(min_length=1)
     destinyCountry: str = Field(min_length=1)
     bagCost: int = Field(ge=0)
-    plannedStartDate: datetime = Field()
-    plannedEndDate: datetime = Field()
+    plannedStartDate: datetime = Annotated[datetime, AfterValidator(date_validation)]
+    plannedEndDate: datetime = Annotated[datetime, AfterValidator(date_validation)]
 
 
-def validate_date(date) -> datetime:
-    """Returns value error if the date is not in the future"""
-    return date < datetime.now()
+def check_uuid4(v: str) -> str:
+    """Returns value error if str is not a valid UUID4"""
+    uuid.UUID(v)
+    return v
+
+
+class CreateRouteResponseSchema(BaseModel):
+    """
+    Response schema used when creating a new route
+    """
+    id: str = Annotated[str, AfterValidator(check_uuid4)]
+    createdAt: str
