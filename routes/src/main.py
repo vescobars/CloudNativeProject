db1323647479683e1696a4d450accf03535fdb16
veronica <v.escobar1@uniandes.edu.co>
2023-08-24 21:59:17 -0500

    [feat] Created connexion with the DB in the main. Connection with local postgres container working.

--- routes/src/main.py
+++ routes/src/main.py
@@ -6,6 +6,8 @@ from src import models
 from src.database import engine
 from src.routes.router import router as routes_router
 
+models.Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
 
 app.include_router(routes_router, prefix="/routes", tags=["Routes"])
