38f701991b6dc04f09aeed382148a7a23fdcd152
veronica <v.escobar1@uniandes.edu.co>
2023-08-24 21:56:55 -0500

    [fix] Modified DB constants to match local values and created now_utc to return current time in utc.

--- routes/src/constants.py
+++ routes/src/constants.py
@@ -1,7 +1,7 @@
 """ File to store global constants and simple utils """
 
 import os
-from datetime import datetime
+from datetime import datetime, timezone
 
 from dotenv import load_dotenv
 
@@ -11,12 +11,17 @@ TOKEN_LENGTH_BYTES = 128
 DEFAULT_SALT_LENGTH_BYTES = 32
 
 DB_USER = os.environ.get("DB_USER", "postgres")
-DB_PASSWORD = os.environ.get("DB_PASSWORD", "password")
-DB_HOST = os.environ.get("DB_HOST", "0.0.0.0")
+DB_PASSWORD = os.environ.get("DB_PASSWORD", 'password')
+DB_HOST = os.environ.get("DB_HOST", "localhost")
 DB_PORT = os.environ.get("DB_PORT", "5432")
-DB_NAME = os.environ.get("DB_NAME", "postgres-cloud")
+DB_NAME = os.environ.get("DB_NAME", "db")
 
 
 def datetime_to_str(date: datetime) -> str:
     """Returns a datetime as string in the correct ISO format"""
     return date.isoformat(sep="T", timespec="seconds").split("+")[0]
+
+
+def now_utc() -> datetime:
+    """Returns the current time in utc"""
+    return datetime.now(timezone.utc)
