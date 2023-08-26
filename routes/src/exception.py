aeccd0888eec9c762db04d5c619d05b70c7dff96
veronica <v.escobar1@uniandes.edu.co>
2023-08-26 13:14:23 -0500

    [feat] Added special exceptions to handle different types of http status codes requested.

---
+++ routes/src/exception.py
@@ -0,0 +1,11 @@
+
+class UniqueConstraintViolatedException(Exception):
+    """A uniqueness constraint in the database was violated"""''
+
+
+class RouteNotFoundException(Exception):
+    """The requested user was not found"""
+
+
+class InvalidRequestException(Exception):
+    """The request body was empty or otherwise invalid"""
