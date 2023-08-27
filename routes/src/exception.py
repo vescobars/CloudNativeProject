dad5dd27288073c2d60afab273fa6c3f12aa3854
veronica <v.escobar1@uniandes.edu.co>
2023-08-27 11:14:35 -0500

    [feat] Added token exceptions to be included later on when auth flow is included.

--- routes/src/exception.py
+++ routes/src/exception.py
@@ -3,10 +3,14 @@
 class UniqueConstraintViolatedException(Exception):
     """A uniqueness constraint in the database was violated"""''
 
-
 class RouteNotFoundException(Exception):
     """The requested user was not found"""
 
-
 class InvalidRequestException(Exception):
     """The request body was empty or otherwise invalid"""
+
+class ExpiredTokenException(Exception):
+    """The token has already expired"""
+
+class InvalidTokenException(Exception):
+    """The token was not found, it might be expired or unauthentic"""
