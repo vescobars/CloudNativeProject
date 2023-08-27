28756cf586a4c0ba16b101d73e3add3bd061ec7a
veronica <v.escobar1@uniandes.edu.co>
2023-08-26 21:26:21 -0500

    [test] Implemented test to verify duplicated flight id exception for create route.

--- routes/tests/routes/test_router.py
+++ routes/tests/routes/test_router.py
@@ -1,4 +1,5 @@
 import datetime
+import json
 import uuid
 import itertools
 import random
@@ -32,7 +33,10 @@ def test_create_route(
         session: Session,
         faker
 ):
-    """"""
+    """
+    Tests create route in a perfect scenario with all correct parameters
+    Expected result is 201 request code and all field assertions stored in db
+    """
     session.execute(
         delete(Route)
     )
@@ -59,9 +63,8 @@ def test_create_route(
 
     response_body = response.json()
     retrieved_route = session.execute(
-        select(Route).where(Route.id == response_body['id'] )
+        select(Route).where(Route.id == response_body['id'])
     ).first()[0]
-
 
     assert retrieved_route.flightId == payload.flightId
     assert retrieved_route.sourceAirportCode == payload.sourceAirportCode
@@ -71,3 +74,62 @@ def test_create_route(
     assert retrieved_route.bagCost == payload.bagCost
     assert retrieved_route.plannedStartDate.replace(tzinfo=timezone.utc) == payload.plannedStartDate
     assert retrieved_route.plannedEndDate.replace(tzinfo=timezone.utc) == payload.plannedEndDate
+
+
+def test_create_route_duplicated_flightId(
+        client: TestClient,
+        session: Session,
+        faker
+):
+    """
+    Tests create route where there is a missing fild in the sent schema.
+    Expected result is 400 request code and all field assertions stored in db
+    """
+    session.execute(
+        delete(Route)
+    )
+    session.commit()
+
+    profile = faker.simple_profile()
+    planned_start_date = now_utc()
+    planned_end_date = (now_utc() + timedelta(days=random.randint(1, 30)))
+
+    payload = CreateRouteRequestSchema(
+        flightId=gen_flightId(),
+        sourceAirportCode=gen_airportCode(),
+        sourceCountry=faker.country(),
+        destinyAirportCode=gen_airportCode(),
+        destinyCountry=faker.country(),
+        bagCost=random.randint(1, 100),
+        plannedStartDate=planned_start_date,
+        plannedEndDate=planned_end_date
+    )
+
+    originalFlightId = payload.flightId
+
+    payload_json = payload.model_dump()
+    response = client.post("/routes", json=payload_json)
+    assert response.status_code == 201
+
+    response_body = response.json()
+    retrieved_route = session.execute(
+        select(Route).where(Route.id == response_body['id'])
+    ).first()[0]
+
+    duplicated_payload = CreateRouteRequestSchema(
+        flightId=originalFlightId,
+        sourceAirportCode=gen_airportCode(),
+        sourceCountry=faker.country(),
+        destinyAirportCode=gen_airportCode(),
+        destinyCountry=faker.country(),
+        bagCost=random.randint(1, 100),
+        plannedStartDate=planned_start_date,
+        plannedEndDate=planned_end_date
+    )
+
+    duplicated_payload_json = payload.model_dump()
+    response = client.post("/routes", json=payload_json)
+    assert response.status_code == 412
+
+
+
