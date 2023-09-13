d1f37a437318077518426c26958d121b67477dfe
coldblade2000 <diegogranada7@gmail.com>
2023-09-12 22:20:17 -0500

    [feat] added exception handler

--- entrega2/src/main.py
+++ entrega2/src/main.py
@@ -4,10 +4,10 @@ from fastapi import FastAPI
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 
-
 from src import models
 from src.config import lifespan
 from src.database import engine
+from src.exceptions import ResponseException
 from src.rf004.router import router as rf004_router
 
 models.Base.metadata.create_all(bind=engine)
@@ -22,4 +22,12 @@ async def validation_exception_handler(_, exc):
     return JSONResponse(status_code=400, content={
         "msg": "Request body is not properly structured",
         "errors": exc.errors()
+    })
+
+
+@app.exception_handler(ResponseException)
+async def response_exception_handler(_, exc: ResponseException):
+    return JSONResponse(status_code=exc.status_code, content={
+        "msg": exc.msg,
+        "detail": exc.detail
     })
