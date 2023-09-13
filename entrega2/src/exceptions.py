3faae603efa6fc3d5e74e94c1dfa71e207a60141
coldblade2000 <diegogranada7@gmail.com>
2023-09-12 19:15:55 -0500

    [fix] fixed 401 and 403 being switched in rf004

--- entrega2/src/exceptions.py
+++ entrega2/src/exceptions.py
@@ -1,17 +1,25 @@
 """ Global Exceptions """
 
 
+class ResponseException(Exception):
+    """Base exception that should be used to return an error response"""
+    status_code: int
+    msg: str = ""
+
+
 class UniqueConstraintViolatedException(Exception):
     """A uniqueness constraint in the database was violated"""''
-
-
-class UtilityNotFoundException(Exception):
-    """The requested utility was not found"""
 
 
 class InvalidRequestException(Exception):
     """The request body was empty or otherwise invalid"""
 
 
-class UnauthorizedUserException(Exception):
+class InvalidCredentialsUserException(ResponseException):
+    """The provided credentials were not valid or missing"""
+    status_code = 403
+
+
+class UnauthorizedUserException(ResponseException):
     """The provided credentials were valid, but were rejected due to lack of authorization or expiration"""
+    status_code = 401
