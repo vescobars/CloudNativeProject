3730df35b56ea87ec3110f07ba0170b7c73244f3
coldblade2000 <diegogranada7@gmail.com>
2023-09-14 04:20:42 -0500

    [feat] finished first draft of RF005

--- entrega2/src/rf005/router.py
+++ entrega2/src/rf005/router.py
@@ -3,9 +3,9 @@
 from fastapi import APIRouter, HTTPException, Response, Request
 
 from src.exceptions import UnauthorizedUserException
-from src.rf004.schemas import CreateOfferResponseSchema
+from src.rf005.schemas import WrappedRF005ResponseSchema, RF005ResponseSchema
 from src.rf005.utils import RF005
-from src.schemas import PostSchema, RouteSchema, CreatedOfferSchema
+from src.schemas import PostSchema, RouteSchema, OfferSchema
 from src.utils import CommonUtils
 
 router = APIRouter()
@@ -23,7 +23,7 @@ def ping():
 @router.get("/posts/{post_id}")
 def find_post(
         post_id: str, request: Request, response: Response,
-) -> CreateOfferResponseSchema:
+) -> WrappedRF005ResponseSchema:
     """
     Returns info about a specific post, its route, and all its offers sorted according to utility
     """
@@ -32,22 +32,22 @@ def find_post(
     post: PostSchema = CommonUtils.get_post(post_id, user_id, full_token)
     route: RouteSchema = CommonUtils.get_route(post.routeId, full_token)
 
-    # TODO create methods to get list of offers
+    detailed_route = RF005.get_detailed_route(route)
 
-    offers = RF005.get_filtered_offers(post.id, full_token)
+    offers: list[OfferSchema] = RF005.get_filtered_offers(post.id, full_token)
 
-    returned_offer = CreatedOfferSchema(
-        id=offer.id,
-        userId=offer.userId,
-        createdAt=offer.createdAt,
-        postId=post.id
+    post_info = RF005ResponseSchema(
+        id=post.id,
+        route=detailed_route,
+        plannedStartDate=route.plannedStartDate,
+        plannedEndDate=route.plannedEndDate,
+        createdAt=post.createdAt,
+        expireAt=post.expireAt,
+        offers=offers,
     )
-    final_response = CreateOfferResponseSchema(
-        data=returned_offer,
-        msg=f"Offer (id={str(offer.id)}) has been successfully created")
 
-    response.status_code = 201
-    return final_response
+    response.status_code = 200
+    return WrappedRF005ResponseSchema(data=post_info)
 
 
 def authenticate(request: Request) -> tuple[str, str]:
