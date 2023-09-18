48c8fbbd0ff32ab7c0258e5290dc69da2630fc33
coldblade2000 <diegogranada7@gmail.com>
2023-09-17 19:35:02 -0500

    [feat] added desperate debugging to environemtn variables

--- entrega2/src/constants.py
+++ entrega2/src/constants.py
@@ -13,7 +13,16 @@ USERS_PATH = os.environ.get("USERS_PATH", "http://localhost:3000")
 ROUTES_PATH = os.environ.get("ROUTES_PATH", "http://localhost:3001")
 POSTS_PATH = os.environ.get("POSTS_PATH", "http://localhost:3002")
 OFFERS_PATH = os.environ.get("OFFERS_PATH", "http://localhost:3003")
-UTILITY_PATH = os.environ.get("UTILITY_PATH", "http://localhost:8001")
+UTILITY_PATH = os.environ.get("UTILITY_PATH", "http://localhost:3004")
+
+print("Connection environment variables")
+print({
+    "USERS_PATH": USERS_PATH,
+    "ROUTES_PATH": ROUTES_PATH,
+    "POSTS_PATH": POSTS_PATH,
+    "OFFERS_PATH": OFFERS_PATH,
+    "UTILITY_PATH": UTILITY_PATH
+})
 
 
 def datetime_to_str(date: datetime) -> str:
