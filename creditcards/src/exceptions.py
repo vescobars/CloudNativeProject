b451a3bd4390b52490b7717d6d1a1c446ae503c4
coldblade2000 <diegogranada7@gmail.com>
2023-09-25 14:32:09 -0500

    [feat] cleaned exceptions

--- creditcards/src/exceptions.py
+++ creditcards/src/exceptions.py
@@ -19,13 +19,6 @@ class UnexpectedResponseCodeException(ResponseException):
     msg = "The expected response status_code was not received."
 
 
-
-class SuccessfullyDeletedOfferException(ResponseException):
-    """Fired after a utility failed to be created, so a corrective offer deletion is successfully issued"""
-    status_code = 500
-    detail = "Utility failed to be stored, offer deleted"
-
-
 class UniqueConstraintViolatedException(ResponseException):
     """A unique constraint has been violated"""
     status_code = 500
@@ -34,46 +27,6 @@ class UniqueConstraintViolatedException(ResponseException):
 
 class InvalidRequestException(Exception):
     """The request body was empty or otherwise invalid"""
-
-
-class OfferInvalidValuesException(ResponseException):
-
-    def __init__(self, original_response):
-        self.detail = original_response
-
-    """The offer has received invalid values"""
-    status_code = 412
-    msg = "The offer has received invalid values"
-
-
-class PostUserOwnerMismatchException(ResponseException):
-    """The user is not the owner of the requested post"""
-    status_code = 403
-    msg = "The user is not the owner of the requested post"
-
-
-class PostIsFromSameUserException(ResponseException):
-    """The requested post is from the same user making an offer"""
-    status_code = 412
-    msg = "The requested post is from the same user making an offer"
-
-
-class PostExpiredException(ResponseException):
-    """The requested post is already expired"""
-    status_code = 412
-    msg = "The requested post is already expired"
-
-
-class PostNotFoundException(ResponseException):
-    """The requested post was not found"""
-    status_code = 404
-    msg = "The requested post doesnt exist"
-
-
-class PostUniqueException(ResponseException):
-    """There is one or more posts that assigned to your route"""
-    status_code = 409
-    msg = "There is one or more posts that assigned to your route"
 
 
 class InvalidParamsException(ResponseException):
@@ -91,39 +44,3 @@ class UnauthorizedUserException(ResponseException):
     """The provided credentials were valid, but were rejected due to lack of authorization or expiration"""
     status_code = 401
     msg = "Credentials were correctly formatted, but were rejected by the server"
-
-
-class RouteStartDateExpiredException(ResponseException):
-    """The route's start date is invalid"""
-    status_code = 412
-    msg = "Las fechas del trayecto no son válidas"
-
-
-class RouteEndDateExpiredException(ResponseException):
-    """The route's end date is invalid"""
-    status_code = 412
-    msg = "Las fechas del trayecto no son válidas"
-
-
-class RouteExpireAtDateExpiredException(ResponseException):
-    """The post's expire at date is invalid"""
-    status_code = 412
-    msg = "La fecha expiración no es válida"
-
-
-class RouteNotFoundException(ResponseException):
-    """The requested route was not found"""
-    status_code = 404
-    msg = "The requested route was not found"
-
-
-class PostFoundInRouteException(ResponseException):
-    """There is already posts with your user in this route"""
-    status_code = 412
-    msg = "El usuario ya tiene una publicación para la misma fecha"
-
-
-class SuccessfullyDeletedRouteException(ResponseException):
-    """Fired after a failed to implement RF003"""
-    status_code = 500
-    detail = "Route failed to be stored, offer deleted"
