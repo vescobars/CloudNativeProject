36887e07b9e6a65e928baa77f3940b52f4dfc8e6
coldblade2000 <diegogranada7@gmail.com>
2023-09-25 02:29:42 -0500

    [feat] Created initial credit card model

--- creditcards/src/models.py
+++ creditcards/src/models.py
@@ -1,15 +1,21 @@
 """ SQLAlchemy models for all global entities """
 
-from sqlalchemy import Column, DateTime, UUID, Float
+from sqlalchemy import Column, DateTime, UUID, String
 
 from src.database import Base
 
 
-class Utility(Base):
-    __tablename__ = "utility"
+class CreditCard(Base):
+    __tablename__ = "CreditCard"
 
-    offer_id = Column(UUID, primary_key=True, index=True, nullable=False)
-    utility = Column(Float, nullable=False)
+    id = Column(UUID, primary_key=True, index=True, nullable=False)
+
+    token = Column(String, nullable=False)
+    userId = Column(UUID, nullable=False)
+    lastFourDigits = Column(String, nullable=False)
+    ruv = Column(String, nullable=False)
+    issuer = Column(String, nullable=False)
+    status = Column(String, nullable=False)
 
     createdAt = Column(DateTime, nullable=False)
     updateAt = Column(DateTime, nullable=False)
