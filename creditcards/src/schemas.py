767bee7e274608d3522db784027bc139712cfd43
coldblade2000 <diegogranada7@gmail.com>
2023-09-25 02:33:49 -0500

    [feat] Created initial credit card schemas

--- creditcards/src/schemas.py
+++ creditcards/src/schemas.py
@@ -1,17 +1,42 @@
 """ Pydantic models for all global entities """
 from datetime import datetime
+from enum import Enum
 
 from pydantic import BaseModel, ConfigDict, UUID4, Field
 
 
-class UtilitySchema(BaseModel):
+class IssuerEnum(Enum):
     """
-    Defines the pydantic model (schema) for Utility
+    The card issuer
+    """
+    VISA = 'VISA'
+    MASTERCARD = 'MASTERCARD'
+    AMERICAN_EXPRESS = 'AMERICAN_EXPRESS'
+    OTHER = 'OTHER'
+
+
+class StatusEnum(Enum):
+    """
+    The card status
+    """
+    POR_VERIFICAR = 'POR_VERIFICAR'
+    RECHAZADA = 'RECHAZADA'
+    APROBADA = 'APROBADA'
+
+
+class CreditCardSchema(BaseModel):
+    """
+    Defines the pydantic model (schema) for CreditCard
     """
     model_config = ConfigDict(from_attributes=True)
 
-    offer_id: UUID4
-    utility: float
+    id: UUID4
+    token: str = Field(max_length=256, min_length=256)
+    userId: UUID4
+    lastFourDigits: str
+    ruv: str
+    issuer: IssuerEnum
+    status: StatusEnum
 
     createdAt: datetime = Field(default_factory=datetime.now)
     updateAt: datetime = Field(default_factory=datetime.now)
