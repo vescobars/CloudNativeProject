4e5e1de5abe2a2e81a064c903cdf058ab8930144
coldblade2000 <diegogranada7@gmail.com>
2023-09-25 14:13:24 -0500

    [fix] restored RF003 router and utils

---
+++ entrega2/src/rf003/router.py
@@ -0,0 +1,103 @@
+from fastapi import APIRouter, HTTPException, Response, Request
+
+from src.exceptions import UnauthorizedUserException, RouteNotFoundException, SuccessfullyDeletedRouteException, \
+    ResponseException
+from src.rf003.schemas import CreateRoutePostRequestSchema, CreatedRouteSchema, CreatePostResponseSchema, \
+    PostWithRouteSchema, CreatedPostSchema
+from src.rf003.utils import RF003
+from src.schemas import RouteSchema
+from src.utils import CommonUtils
+
+router = APIRouter()
+
+
+@router.get("/ping")
+def ping():
+    """
+    Returns "pong" whenever the endpoint is contacted.
+    Functions as a health check
+    """
+    return Response(content="pong", media_type="application/text", status_code=200)
+
+
+@router.post("/posts")
+def create_post(route_data: CreateRoutePostRequestSchema, request: Request,
+                response: Response) -> CreatePostResponseSchema:
+    """
+    Creates a post with the given data.
+    """
+    user_id, full_token = authenticate(request)
+    route_created = False
+    RF003.validate_same_user_or_dates(route_data.plannedStartDate, route_data.plannedEndDate, route_data.expireAt)
+    try:
+        route: RouteSchema = CommonUtils.search_route(route_data.flightId, full_token)
+        RF003.validate_same_user_or_dates(route.plannedStartDate, route.plannedEndDate, route_data.expireAt)
+    except RouteNotFoundException:
+        created_route: CreatedRouteSchema = CommonUtils.create_route(
+            route_data.flightId,
+            route_data.origin.airportCode,
+            route_data.origin.country,
+            route_data.destiny.airportCode,
+            route_data.destiny.country,
+            route_data.bagCost,
+            route_data.plannedStartDate,
+            route_data.plannedEndDate,
+            full_token)
+        route: RouteSchema = RouteSchema(
+            id=created_route.id,
+            flightId=route_data.flightId,
+            sourceAirportCode=route_data.origin.airportCode,
+            sourceCountry=route_data.origin.country,
+            destinyAirportCode=route_data.destiny.airportCode,
+            destinyCountry=route_data.destiny.country,
+            bagCost=route_data.bagCost,
+            plannedStartDate=route_data.plannedStartDate,
+            plannedEndDate=route_data.plannedEndDate,
+            createdAt=created_route.createdAt)
+        route_created = True
+
+    posts = RF003.get_post_filtered(None, route.id, user_id, full_token)
+    if len(posts) == 0:
+        try:
+            post_raw: CreatedPostSchema = CommonUtils.create_post(route.id, route_data.expireAt, full_token)
+            post: PostWithRouteSchema = PostWithRouteSchema(
+                **post_raw.model_dump(),
+                route=CreatedRouteSchema(
+                    id=route.id,
+                    createdAt=route.createdAt
+                ),
+            )
+            final_response = CreatePostResponseSchema(
+                data=post,
+                msg=f"Post (id={str(post.id)}) has been successfully created")
+            response.status_code = 201
+            return final_response
+        except ResponseException as e:
+            print(e)
+            if route_created:
+                RF003.delete_route(route.id, full_token)
+                raise SuccessfullyDeletedRouteException()
+
+    else:
+        RF003.validate_post(posts)
+
+
+def authenticate(request: Request) -> tuple[str, str]:
+    """
+    Checks if authorization token is present and valid, then calls users endpoint to
+    verify whether credentials are still authorized
+
+    Returns the user's id, and the full bearer token present in the
+        authentication header (including the "Bearer " prefix)
+    """
+    if 'Authorization' in request.headers and 'Bearer ' in request.headers.get('Authorization'):
+        full_token = request.headers.get('Authorization')
+        bearer_token = full_token.split(" ")[1]
+        try:
+            user_id = CommonUtils.authenticate_user(bearer_token)
+        except UnauthorizedUserException:
+            raise HTTPException(status_code=401, detail="Unauthorized. Valid credentials were rejected.")
+
+    else:
+        raise HTTPException(status_code=403, detail="No valid credentials were provided.")
+    return user_id, full_token
---
+++ entrega2/src/rf003/utils.py
@@ -0,0 +1,89 @@
+""" Utils for RF003 """
+from datetime import datetime
+from typing import List, Optional
+from uuid import UUID
+
+import requests
+
+from src.constants import POSTS_PATH, ROUTES_PATH
+from src.exceptions import RouteStartDateExpiredException, RouteEndDateExpiredException, \
+    RouteExpireAtDateExpiredException, InvalidParamsException, UnauthorizedUserException, \
+    InvalidCredentialsUserException, PostFoundInRouteException
+from src.schemas import PostSchema
+
+
+class RF003:
+
+    @staticmethod
+    def get_post_filtered(expire: Optional[str], route_id: str, owner: str, bearer_token: str) -> List[PostSchema]:
+        """
+        Retrieves a post from the Posts endpoint
+        :param expire: if the post expires
+        :param route_id: the route's UUID associated with the post
+        :param owner: the post's owner UUID
+        :param bearer_token: the bearer token with which the request is authenticated
+        :return: a post object
+        """
+
+        posts_url = POSTS_PATH.rstrip("/") + "/posts?"
+
+        params = {
+            "expire": expire,
+            "route": route_id,
+            "owner": owner,
+        }
+
+        response = requests.get(posts_url, headers={"Authorization": bearer_token}, params=params)
+        if response.status_code == 404:
+            raise InvalidParamsException()
+        elif response.status_code == 401:
+            raise UnauthorizedUserException()
+        elif response.status_code == 403:
+            raise InvalidCredentialsUserException()
+
+        posts = []
+        for post in response.json():
+            posts.append(PostSchema.model_validate(post))
+
+        return posts
+
+    @staticmethod
+    def delete_route(route_id: UUID, bearer_token: str):
+        """
+        Asks post endpoint to delete post
+        """
+        route_url = ROUTES_PATH.rstrip("/") + f"/routes/{str(route_id)}"
+        requests.delete(route_url, headers={"Authorization": bearer_token})
+
+    @staticmethod
+    def delete_post(post_id: UUID, bearer_token: str):
+        """
+        Asks post endpoint to delete post
+        """
+        posts_url = POSTS_PATH.rstrip("/") + f"/posts/{str(post_id)}"
+        requests.delete(posts_url, headers={"Authorization": bearer_token})
+
+    @staticmethod
+    def validate_post(posts):
+        if len(posts) > 0:
+            raise PostFoundInRouteException()
+
+    @staticmethod
+    def validate_same_user_or_dates(
+            planned_start_date_raw: datetime,
+            planned_end_date_raw: datetime,
+            expire_at_raw: datetime):
+
+        planned_start_date_naive = planned_start_date_raw.replace(tzinfo=None)
+        planned_end_date_naive = planned_end_date_raw.replace(tzinfo=None)
+        expire_at_naive = expire_at_raw.replace(tzinfo=None)
+        dt_now_naive = datetime.utcnow()
+
+        if planned_start_date_naive < dt_now_naive:
+            raise RouteStartDateExpiredException()
+        if planned_end_date_naive < dt_now_naive:
+            raise RouteEndDateExpiredException()
+        if dt_now_naive > expire_at_naive:
+            raise RouteExpireAtDateExpiredException()
+        if expire_at_naive > planned_start_date_naive:
+            raise RouteExpireAtDateExpiredException()
