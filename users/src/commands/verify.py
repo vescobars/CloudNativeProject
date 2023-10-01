0fd91855aaad172f10a4410a6d1ccb5fb89904c3
thalassamania <j.charryg@uniandes.edu.co>
2023-10-01 16:58:20 -0700

    RF007 con Email implementacion

--- users/src/commands/verify.py
+++ users/src/commands/verify.py
@@ -1,9 +1,10 @@
 from .base_command import BaseCommannd
 from ..models.user import User
 from ..session import Session
-from ..errors.errors import IncompleteParams, UserNotFoundError
+from ..errors.errors import IncompleteParams, UserNotFoundError, EmailSendError
 from datetime import datetime, timedelta
 from sqlalchemy import or_
+import requests
 
 
 class VerifyUser(BaseCommannd):
@@ -26,11 +27,31 @@ class VerifyUser(BaseCommannd):
             user.status = 'VERIFICADO'
         else:
             user.status = 'NO_VERIFICADO'
-
         user.last_updated = datetime.utcnow().date()
+
+        response_mail = send_Email(user)
+
+        if response_mail.status_code != 200:
+            raise EmailSendError()
 
         session.commit()
         session.close()
 
     def user_exists(self, session, RUV):
         return len(session.query(User).filter_by(RUV=RUV).all()) > 0
+
+    def send_Email(self, user):
+        url = "https://us-central1-miso-grupo-17.cloudfunctions.net/send_email_notification"
+        headers = {
+            "Content-Type": "application/json",
+        }
+
+        data = {
+            "subject": "Verification Process",
+            "content": f"Your verification process ended with {user.status} status",
+            "recipient": "vescobars2001@gmail.com"
+        }
+
+        response = requests.post(url, headers=headers, json=data)
+
+        return response
